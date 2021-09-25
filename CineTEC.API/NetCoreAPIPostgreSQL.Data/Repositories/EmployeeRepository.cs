fc1aa140df9759a31f1d7b5a2a5235f600509ab8
Shakime <jeykimers.johnson@gmail.com>
2021-09-25 04:56:43 -0600

    [Add] Api2 MVC conexion con postgresql.

---
+++ CineTEC.API/NetCoreAPIPostgreSQL.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,97 @@
+﻿using Dapper;
+using NetCoreAPIPostgreSQL.Model;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPIPostgreSQL.Data.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        // Attributo de configuracion de conexion.
+        private PostgreSQLConfiguration _connectionString;
+
+        // Constructor
+        public EmployeeRepository(PostgreSQLConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Utilizar driver de Nuget para conectarse a la DB.
+        protected NpgsqlConnection dbConnection()
+        {
+            return new NpgsqlConnection(_connectionString.ConnectionString);
+        }
+
+
+        // CRUD
+        public async Task<bool> DeleteEmployee(int id)
+        {
+            var db = dbConnection();
+
+            var sql = @"  DELETE
+                          FROM public.""Employee"" 
+                            WHERE id = @id  ";
+
+            var result =  await db.ExecuteAsync(sql, new { Id = id});
+
+            // TRUE si se elimina almenos una fila.
+            return result > 0;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
+        {
+            var db = dbConnection();
+
+            var sql = @"  SELECT id, username, password, birth_date
+                          FROM public.""Employee"" ";
+
+            return await db.QueryAsync<Employee>(sql, new { });
+
+        }
+
+        public async Task<Employee> GetEmployeeData(int id)
+        {
+            var db = dbConnection();
+
+            var sql = @"  SELECT id, username, password, birth_date
+                          FROM public.""Employee"" 
+                            WHERE id = @id  ";
+
+            return await db.QueryFirstOrDefaultAsync<Employee>(sql, new {Id = id});
+
+        }
+
+        public async Task<bool> InsertEmployee(Employee emp)
+        {
+            var db = dbConnection();
+
+            var sql = @"  INSERT INTO public.""Employee"" (username, password, birth_date)
+                          VALUES (@Username, @Password, @Birth_date) ";
+
+            var result = await db.ExecuteAsync(sql, new { emp.Username, emp.Password, emp.Birth_date });
+
+            // TRUE si se inserta almenos una fila.
+            return result > 0;
+        }
+
+        public async Task<bool> UpdateEmployee(Employee emp)
+        {
+            var db = dbConnection();
+
+            var sql = @"  UPDATE public.""Employee""
+                          SET username   =  @Username,
+                              password   =  @Password,
+                              birth_date =  @Birth_date 
+                            WHERE id = @Id ";
+
+            var result = await db.ExecuteAsync(sql, new { emp.Username, emp.Password, emp.Birth_date });
+            // Devuelve verdadero si al menos mas de una fila ha sido cambiada.
+            return result > 0;
+        }
+
+    }
+}
---
+++ CineTEC.API/NetCoreAPIPostgreSQL/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAPIPostgreSQL.Data.Repositories;
+using NetCoreAPIPostgreSQL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAPIPostgreSQL.Controllers
+{
+    [ApiController]
+    [Route("/cinetec.cr/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            return Ok(await _employeeRepository.GetAllEmployees());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeDetails(int id)
+        {
+            return Ok(await _employeeRepository.GetEmployeeData(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] Employee emp)
+        {
+            if (emp == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _employeeRepository.InsertEmployee(emp);
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee([FromBody] Employee emp)
+        {
+            if (emp == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _employeeRepository.UpdateEmployee(emp);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+
+            await _employeeRepository.DeleteEmployee(id);
+            return NoContent();
+        }
+
+    }
+}
