d1a4b6a57bc7dbc2e7a2ab450aa1bcbf09a03e4e
Jose Alejandro Chavarria <joalchama@gmail.com>
2021-10-10 14:39:25 -0600

    remove SSRS

--- CineTec-WebApp/src/app/app.module.ts
+++ CineTec-WebApp/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
-import { ReportViewerModule } from 'ngx-ssrs-reportviewer';
-
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -13,6 +11,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +27,6 @@ import { HttpClientModule } from '@angular/common/http';
     PagesModule,
     AppRoutingModule,
     HttpClientModule,
-    ReportViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
